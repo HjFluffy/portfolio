35407c3328489a1bd5a7d0a18a240ea8f5f41e13
htinney <hunter.tinney@gmail.com>
2023-08-24 11:10:59 -0600

    Hunter's debugging stuff

--- script/main.js
+++ script/main.js
@@ -1,1 +1,9 @@
-  M.AutoInit();
+/* Hunter's example of initialization.
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems);
+});
+*/
+
+M.AutoInit();
