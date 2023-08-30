bb64a9a999e194d74a3f5acda3181ea58ee128d1
HJFluffy <haydenj989@gmail.com>
2023-08-30 15:11:10 -0600

    Innit
    More placeholder setting and initialization! Im working on getting everything in a home and them designing it later when I finish the HTML and CSS courses for free :)

--- script/main.js
+++ script/main.js
@@ -5,5 +5,20 @@ document.addEventListener('DOMContentLoaded', function() {
   var instances = M.Sidenav.init(elems);
 });
 */
+var coll = document.getElementsByClassName("collapsible");
+var i;
 
-M.AutoInit();
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+    });
+}
+
+
+//M.AutoInit();
